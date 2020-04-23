fd886726f4faf41123e9bdfb5d747de6a19ace35
DavidS <dsabillon.94@gmail.com>
2020-04-23 17:03:18 -0600

    Version1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Resumen from "./components/Resumen";
+import Resultado from "./components/Resultado";
+import Spiner from "./components/Spiner";
+
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+  -webkit-box-shadow: 0px 0px 41px 0px rgba(0, 0, 0, 0.48);
+  -moz-box-shadow: 0px 0px 41px 0px rgba(0, 0, 0, 0.48);
+  box-shadow: 0px 0px 41px 0px rgba(0, 0, 0, 0.48);
+`;
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: "",
+      year: "",
+      plan: "",
+    },
+  });
+
+  const [cargando, guardarCargando] = useState(false);
+
+  // extraer datos
+  const { cotizacion, datos } = resumen;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+
+      <ContenedorFormulario>
+        <Formulario
+          guardarResumen={guardarResumen}
+          guardarCargando={guardarCargando}
+        />
+
+        {cargando ? <Spiner /> : null}
+
+        <Resumen datos={datos} />
+
+        {!cargando ? <Resultado cotizacion={cotizacion} /> : null}
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
